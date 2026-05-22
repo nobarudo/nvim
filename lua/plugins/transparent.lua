2ddf703749d5891589ff72800cff0ef992cc11f9
nobarudo <esukab@gmail.com>
2026-05-23 03:44:38 +0900

    fix:透過設定

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,3 +1,16 @@
 return {
-  { "xiyaowong/transparent.nvim", lazy = false },
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    opts = {
+      -- デフォルトで透過されない Snacks.nvim のグループをここに追加
+      extra_groups = {
+        "SnacksPicker",
+        "SnacksPickerBorder",
+        "SnacksDashboardNormal",
+        "SnacksNotifierNormal",
+        "SnacksNotifierBorder",
+      },
+    },
+  },
 }
